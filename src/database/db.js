0780863fe7c6eb33de5e286edc310b17de73e9f0
thais <thais-pacheco@educar.rs.gov.br>
2025-05-01 18:36:10 -0300

    chore: :rocket: Conexão com o banco de dados SQL

---
+++ src/database/db.js
@@ -0,0 +1,19 @@
+require('dotenv').config();
+const { Sequelize } = require('sequelize');
+
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+  host: process.env.HOST,
+  dialect: process.env.DB_DIALECT,
+  port: process.env.DB_PORT
+});
+
+sequelize.authenticate().then(function(){
+  console.log("Conectado ao banco de dados 🚀");
+}).catch(function(err){
+  console.log("Erro na conexão: "+ err);
+});
+
+module.exports = {
+  Sequelize: Sequelize,
+  sequelize: sequelize,
+}
--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
+require('dotenv').config(); //Variaveis de ambiente
+const sequelize = require('./database/db');
 const express = require("express");
 const app = express();
+
+const port = process.env.PORT;
 
 app.get('/', (req, res) => {
     res.send('API de Gerenciamento de Tarefas está rodando 🚀');
 });
 
-app.listen(3000, () => {
-    console.log(`Servidor rodando em http://localhost:3000`);
+app.listen(port, () => {
+    console.log(`Servidor rodando em http://localhost:${port}`);
 });
